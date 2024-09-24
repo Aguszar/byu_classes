99028569ae5ca0cdbbf56881a5d59446b5e28466
Aguszar <167727105+Aguszar@users.noreply.github.com>
2024-09-23 21:30:33 -0300

    23r23

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Console.Write("What is the magic number ?");
+        string target_inp = Console.ReadLine();
+        int target = int.Parse(target_inp); 
+
+        bool found = false;
+        while (!found){
+            Console.Write("What is your guess ?");
+            string number_input = Console.ReadLine();
+            int number = int.Parse(number_input);
+
+            if(number == target){
+                found = true;
+                Console.Write("You found it !!");
+            }else{
+                if(number < target){
+                    Console.WriteLine("Higher");
+                }else{
+                    Console.WriteLine("Lower");
+                }
+            }
+        }
+
+        
+
+
     }
 }
