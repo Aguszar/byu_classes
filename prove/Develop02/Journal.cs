5e7dc08469223c7e797f5e8641d7373c9074c5e4
Aguszar <167727105+Aguszar@users.noreply.github.com>
2024-10-08 23:33:10 -0300

    Add files via upload

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,29 @@
+class Journal
+{
+    public List<Entry> entries = [];
+
+    public void AddEntry( Entry entry){
+        entries.Add(entry);
+    }
+
+    public void DisplayAll(){
+        
+        for (int i = 0; i < entries.Count; i++){
+            entries[i].Display();
+        }
+    }
+
+    public void SaveToFile(string file){
+        //// FINISH
+
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            // You can add text to the file with the WriteLine method
+            outputFile.WriteLine("This will be the first line in the file.");
+            
+            // You can use the $ and include variables just like with Console.WriteLine
+            string color = "Blue";
+            outputFile.WriteLine($"My favorite color is {color}");
+        }
+    }
+}
