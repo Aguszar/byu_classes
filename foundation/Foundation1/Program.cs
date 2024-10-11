f27877370a36b1662b4765bd0ba663729273a8e0
Aguszar <agustinzarzur4@gmail.com>
2024-10-11 11:26:43 -0300

    foundation1

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -2,8 +2,20 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation1 World!");
+    static void Main(string[] args){
+        
+        Video video = new Video("Funny Cats", "ThatDude", 300);
+
+        Comment c1 = new Comment("user1", "jajjaja");
+        Comment c2 = new Comment("user2", "The orange cat was crazy !!!");
+        Comment c3 = new Comment("user3", "black cats are weird");
+        Comment c4 = new Comment("user4", "keep it up :)");
+
+        video.AddComment(c1);
+        video.AddComment(c2);
+        video.AddComment(c3);
+        video.AddComment(c4);
+        video.ShowAllComments();
+
     }
 }
---
+++ foundation/Foundation1/Video.cs
@@ -0,0 +1,27 @@
+class Video{
+    
+    private string _title;
+    private string _author;
+
+    // seconds
+    private int _length; 
+    List<Comment> _comments = [];
+
+    public Video(string title, string author, int length){
+        _title = title;
+        _author = author;
+        _length = length;
+    }
+
+     public void AddComment(Comment comment){
+        _comments.Add(comment);
+    }
+
+    public void ShowAllComments(){
+        foreach(Comment com in _comments){
+            com.GetUsername();
+            com.GetText();
+            Console.WriteLine();
+        }
+    }
+}
